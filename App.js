4aaddb39c04efd41d997add978694ee883ee2bdd
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2024-07-18 08:20:45 +0530

    Inception Episode 1

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "h1" }, "H1 tag"),
+    React.createElement("h2", { id: "h2" }, "H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "h1" }, "H1 tag"),
+    React.createElement("h2", { id: "h2" }, "H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
